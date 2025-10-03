48867adb314942295f9faca5bc7723d733990f2b
NickRC6 <nickrc6@gmail.com>
2025-10-04 00:36:33 +0700

    Main script starting function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    get_book_text("books/frankenstein.txt")
+
+def get_book_text(path):
+    with open(path, "r", encoding="utf-8") as f:
+        content = f.read()
+    print(content)
+
+main()
+
